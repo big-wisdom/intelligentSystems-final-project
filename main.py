1647c7d8b024ae2b865990cbbd52fa93297e31bd
Eli Hermann <Eli_Hermann@hotmail.com>
2021-12-04 13:56:03 -0700

    clean strings before sending to clisp

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ def main():
         soup = BeautifulSoup(contents, "html.parser")
         ingredients = soup.find_all("li", class_="wprm-recipe-ingredient")
         for ingredient in ingredients:
-            print(ingredient.text)
+            # convert unicode vulgar fractions into floating point numbers
+            print(clean_strings(ingredient.text))
 
         instructions = soup.find_all("li", class_="wprm-recipe-instruction")
         for instruction in instructions:
             print(instruction.text)
+
+
+def clean_strings(text: str) -> str:
+    # replace unicode vulgar fractions with decimal representation
+    text = text.replace(' ½', ".5")
+    text = text.replace(' ¼', ".25")
+    text = text.replace(' ¾', ".75")
+
+    # remove checkboxes
+    text = text.replace('▢', '')
+
+    # remove unnecessary whitespace
+    text = text.strip()
+
+    return text
 
 
 # Press the green button in the gutter to run the script.
